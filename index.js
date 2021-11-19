fe5d101e0a77b95b6b01588fcaf6dd912beb45a3
viki bril <bril2010@gmail.com>
2021-11-19 10:05:38 +0200

    theSecondPartOfOurMission

--- index.js
+++ index.js
@@ -1,10 +1,21 @@
-const http = require ("http");
-const port = 8080;
+const express = require ('express');
+const app = express();
+const port = process.env.PORT || 8080;
 
-http.createServer((req, res) => {
-	res.writeHead(200); // Status code in header
-	res.write("Viki Bril"); // Send res body (can write multiple times)
-	res.end(); // Send & close connection
-}).listen(port); // Listen for connection on this port
+app.all('*', (req, res,next ) => {
+	console.log("Checking the parameters");
+	next();
+});
+app.get('/api/v1/githubUser/:githubUserName/avatar', (req,res) => { res.send("Avatar") });
+app.get('/api/v1/githubUser/:githubUserName/repo/:repoName', (req,res) => { res.send("repoName") });
+app.get('/api/v1/githubUser/:githubUserName/repo/:repoName/contributers', (req,res) => { res.send("contributers") });
+app.get('/profile',
+	(req,res) => {
+		console.log ("There is no spaces between & no extra numbers");
+		res.json("Avatar");
+	}
+);
 
-console.log(`Listening on port ${port}`);
+app.listen(port);
+
+console.log(`Server running on port ${port}`);
